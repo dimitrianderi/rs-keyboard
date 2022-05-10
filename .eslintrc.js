f0bcedcdd94116a9df8521bf103910ad5c3198c6
Dmitry Kharitonov <dimitrianderi@gmail.com>
2022-05-10 17:47:17 +0500

    fix: correct errors of Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": ["airbnb-base"],
+  "env": {
+    "browser": true,
+  },
+  "globals": {
+    "window": true,
+    "document": true
+  },
+  "rules": {
+    "linebreak-style": ["error", "windows"]
+  }
+}
